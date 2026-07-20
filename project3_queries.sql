2dcc4eb4cc682c10fad2e89d2a92d2a1efbd2271
Manasvi Tiwari <manasvitiwari0501@gmail.com>
2026-07-20 20:44:12 +0530

    Add files via upload

---
+++ project3_queries.sql
@@ -0,0 +1,40 @@
+USE decodelabs;
+SHOW TABLES;
+SELECT * FROM orders;
+SELECT Product, Quantity, TotalPrice
+FROM orders;
+SELECT *
+FROM orders
+WHERE OrderStatus = 'Cancelled';
+SELECT Product, TotalPrice
+FROM orders
+ORDER BY TotalPrice DESC;
+SELECT COUNT(*) AS Total_Orders
+FROM orders;
+SELECT Product,
+COUNT(*) AS Orders
+FROM orders
+GROUP BY Product
+ORDER BY Orders DESC;
+SELECT Product,
+SUM(TotalPrice) AS Revenue
+FROM orders
+GROUP BY Product
+ORDER BY Revenue DESC;
+SELECT Product,
+       ROUND(SUM(TotalPrice), 2) AS Revenue
+FROM orders
+GROUP BY Product
+ORDER BY Revenue DESC;
+SELECT AVG(Quantity) AS Average_Quantity
+FROM orders;
+SELECT PaymentMethod,
+COUNT(*) AS Total_Orders
+FROM orders
+GROUP BY PaymentMethod
+ORDER BY Total_Orders DESC;
+SELECT ReferralSource,
+COUNT(*) AS Orders
+FROM orders
+GROUP BY ReferralSource
+ORDER BY Orders DESC;
